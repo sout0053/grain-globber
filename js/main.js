f6f4d8d59082d45c82566631577f10de3d357b52
sout0053 <maxwell.southwood@outlook.com>
2016-03-12 21:57:19 -0500

    Added rest of Javascript

--- js/main.js
+++ js/main.js
@@ -5,4 +5,12 @@ grains.forEach(function (item) {
   var $p = $('<p>');
   var $h2 = $('<h2>');
 
+  $ul.append($li);
+  $img.attr('src', 'images/' + item.img);
+  $li.append($img);
+  $h2.html(item.name);
+  $li.append($h2);
+  $p.html(item.desc);
+  $li.append($p);
+
 });
